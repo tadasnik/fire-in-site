692ff01a78e38dbdf1136bfe5bf39eb0294d328c
tadasnik <tadas.nik@gmail.com>
2023-11-10 15:29:06 +0000

    Fire Char chart png background

---
+++ src/lib/components/visual/FireCharAnotations.svelte
@@ -0,0 +1,55 @@
+<!--
+  @component
+  Generates HTML text labels for a nested data structure. It places the label near the y-value of the highest x-valued data point. This is useful for labeling the final point in a multi-series line chart, for example. It expects your data to be an array of objects where each has `alues` field that is an array of data objects. It uses the `z` field accessor to pull the text label.
+ -->
+<script>
+  import { getContext, tick } from "svelte";
+  import { max } from "d3-array";
+  import { writable } from "svelte/store";
+
+  const { data, x, y, xScale, yScale, xRange, yRange, xDomain } =
+    getContext("LayerCake");
+
+  $: yCoord = max($xDomain) - 400000;
+
+  $: console.log($xDomain, yCoord);
+  $: tickParms = [
+    { label: "1.2m", ros: 1.3, hpa: yCoord },
+    { label: "2.4m", ros: 4.2, hpa: yCoord },
+    { label: "3.5m", ros: 9, hpa: yCoord },
+    { label: "Flame\nlength\n5m", ros: 18.3, hpa: yCoord },
+    { label: "8m", ros: 51, hpa: yCoord },
+  ];
+  $: console.log(tickParms);
+  /* --------------------------------------------
+   * Title case the first letter
+   */
+  const cap = (val) => val.replace(/^\w/, (d) => d.toUpperCase());
+
+  /* --------------------------------------------
+   * Put the label on the highest value
+   */
+  $: left = (value) => $xScale(value) / Math.max(...$xRange);
+  $: top = (value) => $yScale(value) / Math.max(...$yRange);
+</script>
+
+{#each tickParms as tick}
+  <div
+    class="label text-lg"
+    style="
+      top:{top(tick.ros) * 100}%;
+      left:{left(tick.hpa) * 100}%;
+      text-align: right;
+    "
+  >
+    {cap(tick.label)}
+  </div>
+{/each}
+
+<style>
+  .label {
+    position: absolute;
+    transform: translate(-100%, -100%) translateY(1px);
+    font-size: 13px;
+  }
+</style>
---
+++ src/lib/components/visual/FireCharacteristics.svelte
@@ -0,0 +1,76 @@
+<script>
+  import { LayerCake, Svg, Html, groupLonger, flatten } from "layercake";
+
+  import { scaleSequential } from "d3-scale";
+  import {
+    interpolateYlOrRd,
+    interpolateReds,
+    interpolateInferno,
+  } from "d3-scale-chromatic";
+  import { scaleOrdinal } from "d3-scale";
+  import { timeParse, timeFormat } from "d3-time-format";
+  import { format, precisionFixed } from "d3-format";
+  import fireCharBackg from "$lib/assets/fire_char_backg.png";
+
+  import ScatterSvg from "$lib/components/visual/ScatterSvg.svelte";
+  import AxisX from "$lib/components/visual/AxisX.svelte";
+  import AxisY from "$lib/components/visual/AxisY.svelte";
+  import FireCharAnotations from "$lib/components/visual/FireCharAnotations.svelte";
+
+  export let parentWidth;
+  export let data;
+  export let xKey;
+  export let yKey;
+  export let zKey;
+
+  $: flatData = flatten(data, "values");
+</script>
+
+<div
+  class="chart-container"
+  style="width: {parentWidth}; height: {parentWidth};"
+>
+  <LayerCake
+    padding={{ top: 7, right: 10, bottom: 20, left: 25 }}
+    x={xKey}
+    y={yKey}
+    z={zKey}
+    xDomain={[0, 30000000]}
+    xRange={[0, parentWidth - (10 + 25)]}
+    yDomain={[0, 60]}
+    yRange={[parentWidth - 35, 0]}
+    zScale={scaleSequential(interpolateReds)}
+    data={flatData}
+  >
+    <Html>
+      <img alt="The project logo" src={fireCharBackg} />
+    </Html>
+
+    <Svg>
+      <AxisX gridlines={false} ticks={4} snapTicks={true} tickMarks={true} />
+      <AxisY ticks={4} />
+      <ScatterSvg data={flatData} />
+    </Svg>
+
+    <Html>
+      <FireCharAnotations />
+    </Html>
+    <!-- <Html> -->
+    <!--   <Labels /> -->
+    <!--   <SharedTooltip formatTitle={formatTickX} dataset={data} /> -->
+    <!-- </Html> -->
+  </LayerCake>
+</div>
+
+<style>
+  /*
+    The wrapper div needs to have an explicit width and height in CSS.
+    It can also be a flexbox child or CSS grid element.
+    The point being it needs dimensions since the <LayerCake> element will
+    expand to fill it.
+  */
+  .chart-container {
+    width: 100%;
+    height: 100%;
+  }
+</style>
