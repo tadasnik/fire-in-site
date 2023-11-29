c2d8021abc68afb42158b4ec0aa841bcebefef42
tadasnik <tadas.nik@gmail.com>
2023-11-29 08:15:52 +0000

    change contour values

--- src/lib/components/visual/fire_char_backg.py
+++ src/lib/components/visual/fire_char_backg.py
@@ -17,12 +17,15 @@ colors = ["white", "#FFF1EE", "#FFE4DE", "#FFD5CC", "#FFBCAD", "#FE795D"]
 
 colors_gray = ["0.98", "0.9", "0.8", "0.7", "0.6", "0.5"]
 
-flm_thresholds = [-1, 1.2, 2.4, 3.5, 5, 8, 10]
+flm_thresholds = [-1, 0.5, 1.5, 3.5, 5, 8, 10]
 fig, ax = plt.subplots(1, 1, figsize=(10, 10))
-# ax.contour(xv, yv, flm, flm_thresholds, colors="0.5", linewidths=1)
+ax.contour(xv, yv, flm, flm_thresholds, colors="0.5", linewidths=0.1)
 ax.contourf(xv, yv, flm, flm_thresholds, zorder=-1, colors=colors)
-fig.subplots_adjust(left=0, right=1, bottom=0, top=1)
-ax.axis("tight")
-ax.axis("off")
-plt.savefig("../../assets/fire_char_backg.png", dpi=300)
+fig.subplots_adjust(left=0, right=1, bottom=0, top=1.0)
+ax.set_axis_off()
+# ax.axis("tight")
+# ax.axis("off")
+plt.gca().set_position([0, 0, 1, 1])
+plt.savefig("../../assets/fire_char_backg.svg")
+# plt.savefig("../../assets/fire_char_backg.png", dpi=300, bbox_inches="tight")
 plt.show()
