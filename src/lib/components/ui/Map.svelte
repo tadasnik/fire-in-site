d03e58b2ce00f08bfba02ea3e16e9fbe4f8159da
tadasnik <tadas.nik@gmail.com>
2024-06-11 22:29:21 +0100

    slope aspect calcs

--- src/lib/components/ui/Map.svelte
+++ src/lib/components/ui/Map.svelte
@@ -1,6 +1,7 @@
 <script>
   import { onMount, onDestroy } from "svelte";
   import { Map, Marker } from "mapbox-gl";
+  import LatLon from "geodesy/latlon-spherical.js";
   import { locations } from "$lib/shared/stores/locationStore";
   import "$lib/styles/mapbox-gl.css";
 
@@ -12,6 +13,75 @@
   lng = -3;
   lat = 52;
   zoom = 9;
+
+  function slopeAspect(points) {
+    const p1 = new LatLon(
+      points["N"].coordinates._lat,
+      points["N"].coordinates._lon
+    );
+    const p2 = new LatLon(
+      points["S"].coordinates._lat,
+      points["S"].coordinates._lon
+    );
+    const dy = p1.distanceTo(p2);
+
+    const p3 = new LatLon(
+      points["E"].coordinates._lat,
+      points["E"].coordinates._lon
+    );
+    const p4 = new LatLon(
+      points["W"].coordinates._lat,
+      points["W"].coordinates._lon
+    );
+    const dx = p3.distanceTo(p4);
+
+    var dzdx = (points["E"].elevation - points["W"].elevation) / dx,
+      dzdy = (points["N"].elevation - points["S"].elevation) / dy;
+
+    //With those values, slope and aspect are calculated like this:
+
+    const slope = Math.atan(Math.sqrt(dzdx ** 2 + dzdy ** 2)) * (180 / Math.PI);
+    const aspect =
+      dx !== 0 // counterclockwise from east
+        ? (Math.atan2(dzdy, dzdx) * (180 / Math.PI) + 180) % 360
+        : (90 * (dy > 0 ? 1 : -1) + 180) % 360;
+    console.log("another aspect :", 57.29578 * Math.atan2(dzdy, -dzdx));
+    return [slope, aspect];
+  }
+
+  function processPoint(map) {
+    const points = pointsCardinalDirections(
+      $locations.currentLocation.longitude,
+      $locations.currentLocation.latitude,
+      100
+    );
+    pointsElevation(points, map);
+    console.log("slopeAspect", slopeAspect(points));
+  }
+
+  function pointsCardinalDirections(longitude, latitude, dist) {
+    const points = {
+      N: { bearing: 0, coordinates: [], elevation: null },
+      E: { bearing: 90, coordinates: [], elevation: null },
+      S: { bearing: 180, coordinates: [], elevation: null },
+      W: { bearing: 270, coordinates: [], elevation: null },
+    };
+    const p1 = new LatLon(latitude, longitude);
+    Object.keys(points).forEach((key) => {
+      points[key].coordinates = p1.destinationPoint(dist, points[key].bearing);
+    });
+    return points;
+  }
+
+  function pointsElevation(points, map) {
+    Object.keys(points).forEach((key) => {
+      points[key].elevation = map.queryTerrainElevation([
+        points[key].coordinates._lon,
+        points[key].coordinates._lat,
+      ]);
+    });
+    console.log(points);
+  }
 
   onMount(() => {
     const initialState = { lng: lng, lat: lat, zoom: zoom };
@@ -50,6 +120,8 @@
         coordinates.lng,
         coordinates.lat,
       ]);
+      processPoint(map);
+
       console.log("elevation : ", elevation);
     });
     map.setCenter([
@@ -62,6 +134,15 @@
         $locations.currentLocation.latitude,
       ])
       .addTo(map);
+
+    // console.log(
+    //   "pointsCardinalDirections",
+    //   pointsCardinalDirections(
+    //     $locations.currentLocation.longitude,
+    //     $locations.currentLocation.latitude,
+    //     100
+    //   )
+    // );
   });
 
   onDestroy(async () => {
