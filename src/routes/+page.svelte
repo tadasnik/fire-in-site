ee24598d2f0ade0ac6ac5a4d0e9714ac947a1d2e
tadasnik <tadas.nik@gmail.com>
2024-07-09 20:31:29 +0100

    map to left side

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,12 @@
 <script>
   import { Select, Label, Badge, Spinner } from "flowbite-svelte";
+  import { timeFormat } from "d3-time-format";
   import MultiSelect from "$lib/components/ui/MultiSelect.svelte";
   import Map from "$lib/components/ui/Map.svelte";
   import Auth from "$lib/components/Auth.svelte";
   import UKFuelModels from "$lib/data/UKFuelModels.json";
   import SveltyPicker from "svelty-picker";
+  import { simpleNelsonFuelMoisture } from "$lib/model/nelsonFMC/simpleNelson.js";
   import { outputNodes } from "$lib/data/outputNodes.js";
   import {
     selectedOutput,
@@ -32,13 +34,18 @@
     forecastTimeSeries,
     forecastTimeIndex,
     forecastLocation,
+    getForecast,
   } from "$lib/shared/stores/forecastStore.js";
   import { currentLocation } from "$lib/shared/stores/locationStore";
   import { dateTime } from "$lib/shared/stores/timeStore.js";
   import MultiLinePage from "$lib/components/visual/MultiLinePage.svelte";
+  import MultilineTestPage from "$lib/components/visual/MultilineTestPage.svelte";
   import BarFigure from "$lib/components/visual/BarFigure.svelte";
   import FireCharacteristics from "$lib/components/visual/FireCharacteristics.svelte";
+  import InfoTable from "$lib/components/visual/InfoTable.svelte";
+  import Heatmap from "$lib/components/visual/Heatmap.svelte";
   import { getLocation } from "$lib/shared/stores/locationStore";
+  import WeatherInfo from "$lib/components/visual/WeatherInfo.svelte";
 
   export let data;
   let w;
@@ -62,7 +69,7 @@
   // $: console.log("requiredSiteInputsForecast  ", $requiredSiteInputsForecast);
   // $: console.log("inputsForecast  ", $_inputsForecast);
   // $: console.log("outputsForecastArray  ", $_outputForecastArray);
-  $: console.log("forecastTimeIndex  ", $forecastTimeIndex);
+  // $: console.log("forecastTimeIndex  ", $forecastTimeIndex);
   // $: console.log("dateTime :", $dateTime);
   // $: console.log("forecastNow  ", $forecastTimeIndex.get($dateTime));
   // $: console.log("required inputs  ", $requiredSiteInputs);
@@ -74,6 +81,7 @@
   // );
   // $: console.log("fuel inputs", $fuelInputs);
   //
+  const dateFormat = timeFormat("%a %b %e, %H %p");
   const cardinalDirections = {
     0: "N",
     1: "NNE",
@@ -108,76 +116,101 @@
     30: "NNW",
     31: "N",
   };
+  function getWindCardinalDirection(windDir) {
+    return cardinalDirections[Math.floor(windDir / 11.25)].toLowerCase();
+  }
+
+  console.log("$$$$$%$%$%$% index of ");
 </script>
 
-<section class="p-4">
-  <h1>
-    Current fire behaviour prediction for: <strong
-      >{$forecastLocation.name}</strong
-    >
-    <br />
-    <p>
-      lon: {$forecastLocation.coordinates[0].toFixed(3)}, lat: {$forecastLocation.coordinates[1].toFixed(
-        3
-      )}, el.: {$forecastLocation.coordinates[2]}m, slope: {$currentLocation.slope.toFixed(
-        0
-      )}%, aspect: {$currentLocation.aspect.toFixed(0)},
-      {cardinalDirections[Math.floor($currentLocation.aspect / 11.25)]}
+<div class="w-full bg-primary-200">
+  <WeatherInfo
+    data={$forecastTimeIndex.get($dateTime)}
+    forecastLocation={$forecastLocation.name}
+    fireLocation={$currentLocation}
+  />
+</div>
 
-      <br />
-      {new Date($dateTime)}
-    </p>
-  </h1>
-</section>
-<section class="pb-2">
-  <Label for="select-sm" class="mb-2">Select fire behaviour output</Label>
-  <Select id="select-sm" size="sm" class="mb-6" bind:value={$selectedOutput}>
-    {#each $selectedOutputs as output}
-      <option value={output}>{outputNodes[output].label}</option>
-    {/each}
-  </Select>
-</section>
-<div />
-<section class="pt-2">
-  <div class="w-full aspect-square container" bind:clientWidth={w}>
-    {#if $advancedMode}
-      <FireCharacteristics
-        parentWidth={w}
-        data={$_outputForecast.get($dateTime)}
-        xKey="surface.weighted.fire.heatPerUnitArea"
-        yKey="surface.weighted.fire.spreadRate"
-        zKey="surface.primary.fuel.model.catalogKey"
-      />
-    {:else}
-      <BarFigure
-        data={$_outputForecast.get($dateTime)}
-        xKey={$selectedOutput}
-        yKey="surface.primary.fuel.model.catalogKey"
-      />
-    {/if}
-  </div>
-</section>
+<div class="container px-2 mx-auto flex flex-wrap">
+  <div class="sm:w-full md:w-1/2 flex-col px-4 min-w-80">
+    <!-- <div class="sm:w-full md:w-1/2 p-4 min-w-80"> -->
+    <!--   <div class="text-2xl">Fire Location:</div> -->
+    <!--   {$currentLocation.latitude.toFixed(3)}<i -->
+    <!--     class="text-xl wi wi-degrees" -->
+    <!--   />{$currentLocation.latitude >= 0 ? "N" : "S"}, {$currentLocation.longitude.toFixed( -->
+    <!--     3 -->
+    <!--   )}<i class="text-xl wi wi-degrees" />{$currentLocation.longitude >= 0 -->
+    <!--     ? "E" -->
+    <!--     : "W"}, {$currentLocation.elevation.toFixed(0)}m asl, slope: {$currentLocation.slope.toFixed( -->
+    <!--     0 -->
+    <!--   )}%, aspect: -->
+    <!--   <i -->
+    <!--     class="text-2xl wi wi-wind -->
+    <!--   wi-towards-{getWindCardinalDirection($currentLocation.aspect)}" -->
+    <!--   /> -->
+    <!--   <strong -->
+    <!--     >({cardinalDirections[ -->
+    <!--       Math.floor($currentLocation.aspect / 11.25) -->
+    <!--     ]})</strong -->
+    <!--   > -->
+    <!-- </div> -->
 
-<section class="pt-5">
-  <div class="w-full" bind:clientWidth={w}>
-    <MultiLinePage
-      data={$_outputForecastArray}
-      xKey="time"
-      yKey="value"
-      zKey="surface.primary.fuel.model.catalogKey"
-    />
-  </div>
-</section>
-
-<section class="pt-5">
-  <div class="w-full aspect-square container" bind:clientWidth={w}>
-    <section>
+    <div class="aspect-square container" bind:clientWidth={w}>
       {#await getLocation()}
         <div class="text-center"><Spinner /></div>
       {:then}
         <Map />
       {/await}
-    </section>
+    </div>
+  </div>
+
+  <div class="sm:w-full md:w-1/2 p-4 min-w-80">
+    <Label for="select-sm" class="mb-2">Select fire behaviour output</Label>
+    <Select id="select-sm" size="sm" class="mb-6" bind:value={$selectedOutput}>
+      {#each $selectedOutputs as output}
+        <option value={output}>{outputNodes[output].label}</option>
+      {/each}
+    </Select>
+    <div />
+    <div class="container" bind:clientWidth={w}>
+      {#if $advancedMode}
+        <FireCharacteristics
+          parentWidth={w}
+          data={$_outputForecast.get($dateTime)}
+          xKey="surface.weighted.fire.heatPerUnitArea"
+          yKey="surface.weighted.fire.spreadRate"
+          zKey="surface.primary.fuel.model.catalogKey"
+        />
+      {:else if $_outputForecast.get($dateTime)}
+        <BarFigure
+          data={$_outputForecast}
+          time={$dateTime}
+          xKey={$selectedOutput}
+          yKey="surface.primary.fuel.model.catalogKey"
+        />
+      {/if}
+    </div>
+  </div>
+</div>
+<section class="pt-5">
+  <div class="w-full" bind:clientWidth={w}>
+    <MultiLinePage
+      data={$_outputForecastArray}
+      xKey="time"
+      yKey={$selectedOutput}
+      zKey="surface.primary.fuel.model.catalogKey"
+    />
+  </div>
+</section>
+<section class="pt-5">
+  <div class="w-full overflow-x-auto" bind:clientWidth={w}>
+    <Heatmap
+      data={$_outputForecastArray}
+      forecastData={$forecastTimeSeries}
+      xKey="time"
+      zKey={$selectedOutput}
+      yKey="surface.primary.fuel.model.catalogKey"
+    />
   </div>
 </section>
 
@@ -229,4 +262,12 @@
 </section>
 
 <style>
+  .xcontainer {
+    scrollbar-width: none; /* Firefox */
+    -ms-overflow-style: none; /* IE 10+ */
+  }
+  .xcontainer::-webkit-scrollbar {
+    /* WebKit */
+    width: 0px;
+  }
 </style>
