2d200fe6f8a797b050e88b92f2ea56b830c469f1
tadasnik <tadas.nik@gmail.com>
2024-07-20 10:10:04 +0100

    vite manage images

--- src/lib/components/visual/Bars.svelte
+++ src/lib/components/visual/Bars.svelte
@@ -4,56 +4,53 @@
  -->
 <script>
   import { onMount, getContext } from "svelte";
+
   import {
     getFuelsImages,
     getFuelsFileNames,
   } from "$lib/firebase/firebase.client";
   import UKFuelModels from "$lib/data/UKFuelModels.json";
-
+  const images = import.meta.glob(["$lib/assets/bars-background/**.webp"], {
+    eager: true,
+    as: "url",
+  });
   const { data, xGet, yGet, xScale, yScale, xRange } = getContext("LayerCake");
 
-  /** @type {String} [fill='#00bbff'] - The shape's fill color. This is technically optional because it comes with a default value but you'll likely want to replace it with your own color. */
-  // export let fill = "#00bbff";
-
-  let imageNames;
-  let images = {};
-
   const xDomain = [0, 100];
-  onMount(async () => {
-    imageNames = await getFuelsFileNames();
-    images = await getFuelsImages(imageNames);
-  });
 </script>
 
 <g class="bar-group">
   {#each $data as d, i}
-    {#await images[UKFuelModels[d["surface.primary.fuel.model.catalogKey"]].photo.split(".")[0]] then imageUrl}
-      <defs>
-        <pattern
-          id={i}
-          x={$xScale.range()[0]}
-          y={$yGet(d.values[0])}
-          patternUnits="userSpaceOnUse"
-          width={$xGet(d.values[0])}
-          height={$yScale.bandwidth()}
-        >
-          <image xlink:href={imageUrl} y={-$yScale.bandwidth() * 2} />
-        </pattern>
-      </defs>
-
-      <rect
-        class="group-rect"
-        data-id={i}
+    <defs>
+      <pattern
+        id={i}
         x={$xScale.range()[0]}
         y={$yGet(d.values[0])}
+        patternUnits="userSpaceOnUse"
+        width={$xGet(d.values[0])}
         height={$yScale.bandwidth()}
-        width={$xGet(d.values[0]) === 0 ? 1 : $xGet(d.values[0])}
-        fill="url(#{i})"
-        stroke-width="1"
-        stroke="grey"
-      />
-    {:catch error}
-      <p style="color: red">{error.message}</p>
-    {/await}
+      >
+        <image
+          xlink:href={images[
+            "/src/lib/assets/bars-background/" +
+              UKFuelModels[d["surface.primary.fuel.model.catalogKey"]].photo
+          ]}
+          y={-$yScale.bandwidth() * 2}
+        />
+      </pattern>
+    </defs>
+
+    <rect
+      class="group-rect"
+      id={"rect-" + i}
+      data-id={i}
+      x={$xScale.range()[0]}
+      y={$yGet(d.values[0])}
+      height={$yScale.bandwidth()}
+      width={$xGet(d.values[0]) === 0 ? 1 : $xGet(d.values[0])}
+      fill="url(#{i})"
+      stroke-width="1"
+      stroke="grey"
+    />
   {/each}
 </g>
