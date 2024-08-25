91ff2436c3af8146fb6322db120461416c5e088f
tadasnik <tadas.nik@gmail.com>
2024-08-25 16:23:45 +0100

    remove enhanced-img

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import { purgeCss } from 'vite-plugin-tailwind-purgecss';
 import { sveltekit } from '@sveltejs/kit/vite';
-import { enhancedImages } from '@sveltejs/enhanced-img';
 import dsv from '@rollup/plugin-dsv'
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [sveltekit(), purgeCss(), dsv(), enhancedImages()]
+  plugins: [sveltekit(), purgeCss(), dsv()]
 });
