539f7cffdd6a00084a428e81092e8cdb1e93cb89
tadasnik <tadas.nik@gmail.com>
2024-08-25 16:06:25 +0100

    tidy imports

--- src/lib/shared/stores/timeStore.js
+++ src/lib/shared/stores/timeStore.js
@@ -1,4 +1,5 @@
 import { writable, derived } from 'svelte/store'
+import { timeFormat } from 'd3-time-format'
 
 export function differenceHours(date1, date2) {
   return (date1 - date2) / 3600000
@@ -19,6 +20,12 @@ export const currentDateTime = writable(
   new Date()
 )
 
+export const dateString = derived(currentDateTime, ($currentDateTime) => {
+  return timeFormat('%Y-%m-%d')($currentDateTime)
+})
+
+export const timeMode = writable('current')
+
 export const dateTime = derived(
   currentDateTime, ($currentDateTime) => {
     return 3600000 * (Math.round($currentDateTime / 3600000))
@@ -37,3 +44,12 @@ export const hour = derived(
     return $currentDateTime.getHours() + 1;
 
   })
+
+export const historicalYear = writable()
+export const historicalMonth = writable()
+export const historicalDay = writable()
+export const historicalDate = derived([historicalYear, historicalMonth, historicalDay], ([$historicalYear, $historicalMonth, $historicalDay]) => {
+  console.log("historicalDate", $historicalYear, $historicalMonth, $historicalDay)
+  return new Date($historicalYear, $historicalMonth, $historicalDay, 12, 0)
+})
+
