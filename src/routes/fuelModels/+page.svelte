42c89b2254971019cc9c34797356da7abc3dd1f5
tadasnik <tadas.nik@gmail.com>
2024-09-01 16:31:58 +0100

    improved fuels table

--- src/routes/fuelModels/+page.svelte
+++ src/routes/fuelModels/+page.svelte
@@ -7,27 +7,57 @@
     TableHead,
     TableHeadCell,
     ImagePlaceholder,
+    Card,
     Modal,
+    Gallery,
   } from "flowbite-svelte";
   import UKFuelModels from "$lib/data/UKFuelModels.json";
   import { fuelNodes } from "$lib/data/fuelNodes.js";
+  import FuelModelModal from "$lib/components/visual/FuelModal.svelte";
 
   import { slide } from "svelte/transition";
 
-  const showItems = [
-    "label",
-    "code",
-    "depth",
-    "dead1Load",
-    "dead10Load",
-    "dead100Load",
-    "totalHerbLoad",
-    "liveStemLoad",
-    "dead1Savr",
-    "liveHerbSavr",
-    "liveStemSavr",
-    "deadMext",
-  ];
+  import { selectedFuels } from "$lib/shared/stores/modelStore";
+
+  const fuelsImages = import.meta.glob("$lib/assets/fuelModelPhotos/*.webp", {
+    eager: true,
+    as: "url",
+  });
+
+  function getFuelImages(fuel) {
+    let images = [];
+    Object.entries(fuelsImages).forEach(([key, value]) => {
+      if (key.split("/")[5].split("_")[0] === fuel.toLowerCase()) {
+        images.push({ src: value, alt: fuel });
+      }
+    });
+    return images;
+  }
+
+  function getFuelImage(fuel) {
+    return fuelsImages[`$lib/assets/fuelModelPhotos/${fuel}*.webp`];
+  }
+
+  const showItems = {
+    depth: "surface.primary.fuel.model.behave.parms.depth",
+    dead1Load: "surface.primary.fuel.model.behave.parms.dead.tl1h.ovendryLoad",
+    dead10Load:
+      "surface.primary.fuel.model.behave.parms.dead.tl10h.ovendryLoad",
+    dead100Load:
+      "surface.primary.fuel.model.behave.parms.dead.tl100h.ovendryLoad",
+    totalHerbLoad:
+      "surface.primary.fuel.model.behave.parms.total.herb.ovendryLoad",
+    liveStemLoad:
+      "surface.primary.fuel.model.behave.parms.live.stem.ovendryLoad",
+    dead1Savr:
+      "surface.primary.fuel.model.behave.parms.dead.tl1h.surfaceAreaToVolumeRatio",
+    liveHerbSavr:
+      "surface.primary.fuel.model.behave.parms.live.herb.surfaceAreaToVolumeRatio",
+    liveStemSavr:
+      "surface.primary.fuel.model.behave.parms.live.stem.surfaceAreaToVolumeRatio",
+    deadMext:
+      "surface.primary.fuel.model.behave.parms.dead.extinction.moistureContent",
+  };
 
   let openRow;
   let details;
@@ -36,47 +66,67 @@
   const toggleRow = (i) => {
     openRow = openRow === i ? null : i;
   };
-  console.log("uk fuel models", UKFuelModels);
 </script>
 
 <Table>
   <TableHead>
-    {#each showItems as item}
-      <TableHeadCell>{item}</TableHeadCell>
+    <TableHeadCell class="text-sm">Fuel code</TableHeadCell>
+    {#each Object.entries(showItems) as [key, item]}
+      <TableHeadCell class="text-sm normal-case"
+        >{fuelNodes[item].label}
+        <span class="text-xs font-normal">({fuelNodes[item].units})</span
+        ></TableHeadCell
+      >
     {/each}
   </TableHead>
   <TableBody tableBodyClass="divide-y">
-    {#each Object.entries(UKFuelModels) as [id, fuel], i}
+    {#each $selectedFuels as fuel, i}
       <TableBodyRow on:click={() => toggleRow(i)}>
-        {#each showItems as item}
-          <TableBodyCell>{fuel[item]}</TableBodyCell>
+        <TableBodyCell class="text-sm">{UKFuelModels[fuel].code}</TableBodyCell>
+        {#each Object.entries(showItems) as [key, item]}
+          <TableBodyCell>{UKFuelModels[fuel][key]}</TableBodyCell>
         {/each}
       </TableBodyRow>
       {#if openRow === i}
+        {@const images = getFuelImages(fuel)}
         <TableBodyRow
           on:dblclick={() => {
             doubleClickModal = true;
-            details = item;
+            details = i;
           }}
         >
-          <TableBodyCell colspan="4" class="p-0">
+          <TableBodyCell colspan="11" class="p-0">
             <div
               class="px-2 py-3"
               transition:slide|global={{ duration: 300, axis: "y" }}
             >
-              <ImagePlaceholder />
-            </div>
-          </TableBodyCell>
+              <div class="flex flex-col md:flex-row">
+                {#if images.length !== 0}
+                  {#each images as image}
+                    <div class="p-2 min-w-96 min-h-60">
+                      <img
+                        class="object-cover h-60 w-96"
+                        src={image.src}
+                        alt={image.alt}
+                      />
+                    </div>
+                  {/each}
+                {/if}
+                <div class="p-8">
+                  <div
+                    class="uppercase tracking-wide text-sm text-primary-800 font-semibold"
+                  >
+                    {UKFuelModels[fuel].label}
+                  </div>
+                  <p class="mt-2 text-wrap max-w-96 text-slate-500">
+                    {UKFuelModels[fuel].description}
+                  </p>
+                </div>
+              </div>
+            </div></TableBodyCell
+          >
         </TableBodyRow>
       {/if}
     {/each}
   </TableBody>
 </Table>
-<Modal
-  title={details?.name}
-  bind:open={doubleClickModal}
-  autoclose
-  outsideclose
->
-  <ImagePlaceholder />
-</Modal>
