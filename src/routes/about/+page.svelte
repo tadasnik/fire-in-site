13d63a7cad0b9bd56f1ebfa343035d80aa4973f1
tadasnik <tadas.nik@gmail.com>
2024-10-28 15:50:34 +0000

    about placeholder

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+  import { Card, Modal, Gallery } from "flowbite-svelte";
+  import { Heading, P, A, Mark, Secondary } from "flowbite-svelte";
+</script>
+
+<div class="container mx-auto text-center p-10">
+  <Heading>FireInSite</Heading>
+  <div class="p-4">
+    <p>
+      FireInSite is a fire behaviour prediction system in the form of a
+      web-based application that forecasts the probability of ignition, surface
+      fire rate of spread, flame length and fireline intensity for a user
+      selected location for a set of core UK fire prone vegetation types
+      (fuels).
+    </p>
+  </div>
+</div>
