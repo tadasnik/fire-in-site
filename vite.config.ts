2ca9891f99455df9184872345e408cdfd2e52125
tadasnik <tadas.nik@gmail.com>
2024-11-05 06:36:59 +0000

    onnx model

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
 import { purgeCss } from 'vite-plugin-tailwind-purgecss';
 import { sveltekit } from '@sveltejs/kit/vite';
+import wasm from "vite-plugin-wasm";
+import topLevelAwait from "vite-plugin-top-level-await";
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [sveltekit(), purgeCss()]
+  plugins: [sveltekit(), purgeCss(), wasm(), topLevelAwait()],
+optimizeDeps: {
+        exclude: ['onnxruntime-web'], // Exclude onnxruntime-web from pre-bundling
+    },
+    build: {
+        target: 'esnext',
+        rollupOptions: {
+            // Ensure .wasm files are correctly handled and imported
+            output: {
+                manualChunks: undefined,
+            },
+        },
+    },
+    // Add custom handling for WASM files
+    assetsInclude: ['**/*.wasm'],
 });
