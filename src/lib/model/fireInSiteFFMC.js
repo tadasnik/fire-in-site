5bdc9aff63d857e1b3ca6a97a732b1aadf7a0e41
tadasnik <tadas.nik@gmail.com>
2024-11-05 06:26:28 +0000

    fireInSite dead ffmc

--- src/lib/model/fireInSiteFFMC.js
+++ src/lib/model/fireInSiteFFMC.js
@@ -1,5 +1,4 @@
 import * as ort from 'onnxruntime-web';
-
 // use an async context to call onnxruntime functions.
 export async function deadFFMC(inputs) {
   try {
@@ -8,40 +7,21 @@ export async function deadFFMC(inputs) {
     // the model in this example contains a single MatMul node
     // it has 2 inputs: 'a'(float32, 3x4) and 'b'(float32, 4x3)
     // it has 1 output: 'c'(float32, 3x3)
-    const modelUrl = "/wasm/mono_model.onnx";
+    const modelUrl = "/wasm/model_onehot_dead.onnx";
     const session = await ort.InferenceSession.create(modelUrl, {
       wasm: {
         wasmPaths: '/wasm/ort-wasm-simd-threaded.wasm',
       }
     }
     )
-    console.log('ONNX model loaded successfully', inputs);
-    // console.log('Model metadata:', session);
-    // console.log('Model input metadata:', session.inputNames);
-    // console.log('Model output metadata:', session.outputNames);
-    // prepare inputs. a tensor need its corresponding TypedArray as data
+    // console.log('ONNX model loaded successfully', inputs);
     const dataA = Float32Array.from(inputs)
-    // const dataA = Float32Array.from([1.08401656e-01, 1.98291779e-01, 3.01038504e-01, 4.76410031e-01,
-    //   6.02268219e-01, 9.80200806e+01, 2.12419464e+02, 3.59098389e+02,
-    //   4.30481415e+02, 4.00864166e+02, 1.88428473e+00, 2.31000042e+01,
-    //   3.75000000e+02, 8.00000000e+00, 3.00000000e+00]);
-    const tensorA = new ort.Tensor('float32', dataA, [dataA.length / 15, 15]);
-
-    // const dataA = Float32Array.from([1.08401656e-01, 1.98291779e-01, 3.01038504e-01, 4.76410031e-01,
-    //   6.02268219e-01, 9.80200806e+01, 2.12419464e+02, 3.59098389e+02,
-    //   4.30481415e+02, 4.00864166e+02, 1.88428473e+00, 2.31000042e+01,
-    //   3.75000000e+02, 8.00000000e+00, 3.00000000e+00]);
-    // const tensorA = new ort.Tensor('float32', dataA, [15]);
-
-    // prepare feeds. use model input names as keys.
+    const tensorA = new ort.Tensor('float32', dataA, [inputs.length / 18, 18]);
+    // console.log('tensorA:', tensorA);
     const feeds = { X: tensorA };
-
-    // feed inputs and run
     const results = await session.run(feeds);
-
-    // read from results
     const dataC = results.variable.cpuData;
-    console.log("data of result tensor 'c':", dataC);
+    // console.log("data of result tensor 'c':", dataC);
 
     return results.variable.cpuData
   } catch (e) {
--- src/lib/model/fuelMoisture.js
+++ src/lib/model/fuelMoisture.js
@@ -2,10 +2,11 @@ import { getMonth } from '$lib/shared/stores/timeStore'
 import { simpleNelsonFuelMoisture } from '$lib/model/nelsonFMC/simpleNelson';
 import { deadFFMC } from './fireInSiteFFMC';
 
-// slice array and pad with zeros if the slice is
-// less than the target length (padLength)
+
+const deadFuelsCategories = { 0: "bracken", 1: "gorse", 2: "heather", 3: "grass" }
 function getPaddedSlice(arr, start, end, padLength) {
   // Get the base slice
+  // console.log(start, end, padLength)
   let result;
   if (end === 0) {
     result = [];
@@ -14,43 +15,71 @@ function getPaddedSlice(arr, start, end, padLength) {
   } else {
     result = arr.slice(start, end);
   }
-  // Pad to reach target length
+
+  // Pad or trim to reach target length
   if (result.length < padLength) {
     return [...new Array(padLength - result.length).fill(0), ...result];
-  }
+  } else { return result; }
 }
 
 export async function fuelMoistureCalcs(forecast, timeLag, slope, aspect, elevation) {
-  console.log("forecast", forecast)
-
   let nelsonFFMC = null
   const nelsonFFMCs = []
   const fsFFMCs = []
   const ffmcInputs = []
-  const ffmcInputTimes = []
+  // const ffmcInputTimes = []
+  const deadFuelsCount = Object.keys(deadFuelsCategories).length
+  // console.log("fuel moisture calc forecast", forecast, timeLag, slope, aspect, elevation)
+
   forecast.time.forEach((time, nr) => {
     nelsonFFMC = simpleNelsonFuelMoisture(nelsonFFMC, forecast["temperature2m"][nr], forecast["globalTiltedIrradiance"][nr], forecast["relativeHumidity2m"][nr], forecast["precipitation"][nr])
     nelsonFFMCs.push(nelsonFFMC)
-    const inputs = ([...getPaddedSlice(forecast["vapourPressureDeficit"], nr - timeLag, nr + 1, timeLag),
-    ...getPaddedSlice(forecast["globalTiltedIrradiance"], nr - timeLag, nr + 1, timeLag),
-      slope, aspect, elevation, getMonth(new Date(time)), 1.])
-    console.log("inputs", new Date(time), inputs)
+    nr = nr + 1
+    const inputs = ([...getPaddedSlice(forecast["vapourPressureDeficit"], nr - timeLag, nr, timeLag),
+    ...getPaddedSlice(forecast["globalTiltedIrradiance"], nr - timeLag, nr, timeLag),
+      slope, aspect, elevation, getMonth(new Date(time))])
+    for (let i = 0; i < deadFuelsCount; i++) {
+      let fuelFlagArray = Array(deadFuelsCount).fill(0.)
+      fuelFlagArray[i] = 1
+      ffmcInputs.push(...inputs, ...fuelFlagArray)
+    }
+    // console.log("inputs", new Date(time), inputs)
 
-    ffmcInputs.push(...inputs)
-    ffmcInputTimes.push(time)
+    // ffmcInputs.push(...inputs)
+    // ffmcInputTimes.push(time)
   })
+  // console.log("ffmcInputs", ffmcInputs)
   try {
     // First promise
+    // console.log("inputs to ffmc", ffmcInputs)
     const resultMoist = await deadFFMC(
       ffmcInputs
     );
-    forecast["ffmc"] = resultMoist
+    // console.log("resultMoist", resultMoist)
+    for (let i = 0; i < deadFuelsCount; i++) {
+      let values = []
+      for (let x = i; x < resultMoist.length; x = x + deadFuelsCount) {
+        values.push(resultMoist[x]);
+      }
+      forecast["ffmc" + "_" + deadFuelsCategories[i]] = values
+    }
+
+    // const chunkSize = resultMoist.length / deadFuelsCount
+    // for (let i = 0; i < resultMoist.length; i += chunkSize) {
+    //   console.log("i", i, i + chunkSize)
+    //   const chunk = resultMoist.slice(i, i + chunkSize);
+    //   console.log("chunk", chunk, chunk.length)
+    //   forecast["ffmc" + "_" + deadFuelsCategories[i]] = chunk
+    // do whatever
+    // }
+    // forecast["ffmc"] = resconst chunkSize = resultMoist.length / deadFuelsCount)
 
   } catch (error) {
     console.error("Error fetching forecast:", error);
     fetchingForecast.set(false);
   }
-  forecast["nelsonFFMC"] = nelsonFFMCs
+  forecast["ffmc_nelson"] = nelsonFFMCs
+  // console.log("forecast", forecast)
   return forecast
 
 }
