aa20a9dbc19354af12b2f10bff7c2e8b94270166
tadasnik <tadas.nik@gmail.com>
2025-01-24 16:17:38 +0000

    user inputs changes

--- src/lib/model/surfaceFireOptimized.js
+++ src/lib/model/surfaceFireOptimized.js
@@ -54,10 +54,12 @@ export default class FireSim {
     this.dag.setUpdateClass(new UpdateRandomOrthogonalStack(this.dag))
     // Set display units
     for (const [key, values] of Object.entries(nodeProps)) {
-      this.dag.get(key)._variant.setDisplayUnits(values.units)
+      if (values.selected === true) {
+        this.dag.get(key)._variant.setDisplayUnits(values.units)
+      }
+      // console.log(this.dag.get('surface.primary.fuel.model.behave.parms.dead.tl1h.surfaceAreaToVolumeRatio'))
+      // console.log(this.dag.get('site.temperature.shading'))
     }
-    // console.log(this.dag.get('surface.primary.fuel.model.behave.parms.dead.tl1h.surfaceAreaToVolumeRatio'))
-    // console.log(this.dag.get('site.temperature.shading'))
   }
 
   selectOutputs(outputs) {
@@ -95,17 +97,60 @@ export default class FireSim {
     return resultsArray
   }
 
+  /**
+   * Calculate the critical surface fire intensity (I'initiation)
+   * sufficient to drive off canopy foliar moisture and initiate a
+   * passive or active crown fire.
+   *
+   * This is Scott & Reinhardt (2001) equation 11 (p 13).
+   *
+   * @param folMois Canopy foliar moisture content (percent).
+   * @param cpyBase Crown canopy base height (m).
+   * @return The critical surface fireline intensity (btu+1 ft-1 s-1)
+   *  required to initiate a passive or active crown fire.
+   */
+  fliInit(folMois, cpyBase) {
+    const fmc = Math.max(30, 100 * folMois) // convert to percent with 30% min
+    const cbh = Math.max(0.1, cpyBase) // with 10 cm min
+    const kwm = Math.pow(0.01 * cbh * (460 + 25.9 * fmc), 1.5) // (kW/m)
+    return kwm * 0.288672 // return as Btu/ft/s
+  }
+
+  /**
+   * Calculate conopy base height at which transition to canopy ratio is 1.
+   * Below this height transition to canopy is likely.
+   *
+   * @param intensit surface fireline intensity (kw/m)
+   * @param folMois Canopy foliar moisture content (percent).
+   *
+  */
+  cbhInit(intensity, folMois) {
+    const denominator = 0.01 * (460 + 25.9 * folMois)
+    const nominator = Math.pow(intensity * 1000, 2 / 3)
+    const cbh = nominator / denominator
+    return cbh
+  }
+
   runBasic(inputs) {
     this.dag.setRunLimit(1000)
     this.dag.setUpdateClass(new UpdateOrthogonalStack(this.dag))
     const inputsArray = []
     for (const [key, values] of Object.entries(inputs)) {
-      inputsArray.push([key, this.arrayToNative(key, values)])
+      if (Array.isArray(values)) {
+        // console.log('array: ', key, values)
+        // console.log('pushing ', [key, this.arrayToNative(key, values)[0]])
+        inputsArray.push([key, this.arrayToNative(key, values)])
+      } else {
+        // console.log('not array: ', key, values)
+        // console.log('pushing ', [key, this.dag.get(key)._variant.displayValueToNativeValue(values)])
+        inputsArray.push([key, this.dag.get(key)._variant.displayValueToNativeValue(values)])
+      }
     }
     this.dag.input(inputsArray)
     this.dag.run()
     return this.store._resultsArray
   };
+
 
   runWithRandom(inputs) {
     this.dag.setRunLimit(1000)
@@ -166,9 +211,15 @@ export default class FireSim {
   arrayToNative(key, values) {
     const valuesNative = []
     const node = this.dag.get(key)
+    // console.log('key: ', key, 'values: ', values)
     values.forEach((element) => {
+      // if (key === 'site.canopy.crown.baseHeight') {
+      //   console.log('valuesNative: ', values, element)
+      // }
+
       valuesNative.push(node._variant.displayValueToNativeValue(element))
     })
+
     return valuesNative
   }
 }
