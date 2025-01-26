6ad17a1bd05c6f7b7f90840a639eb22da54ab9cc
tadasnik <tadas.nik@gmail.com>
2025-01-26 13:49:35 +0000

    output select into component

---
+++ src/lib/components/ui/SelectOutput.svelte
@@ -0,0 +1,20 @@
+<script>
+  import { Select } from "flowbite-svelte";
+  import {
+    selectedOutput,
+    selectedOutputs,
+  } from "$lib/shared/stores/modelStore";
+  import { outputNodes } from "$lib/data/outputNodes.js";
+  $: outputs = [...$selectedOutputs, "crown.fire.transition.minBaseHeight"];
+</script>
+
+<div>
+  <Select id="select-output" size="sm" bind:value={$selectedOutput}>
+    {#each outputs as output}
+      <option value={output}
+        >{outputNodes[output].label}
+        ({outputNodes[output].displayUnits})</option
+      >
+    {/each}
+  </Select>
+</div>
